9fe4b93b84c31ee236f0d74254748488d31c35aa
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 13:00:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-# streamlit_app.py
-
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+from sklearn.neighbors import NearestNeighbors
+import pyodbc
+import pymysql
 
-# Initialize connection.
-conn = st.experimental_connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from df;', ttl=600)
+#creating connection
+engine = sqlalchemy.create_engine(
+    "mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
+    )
 
-st.dataframe(df)
+@st.cache(allow_output_mutation=True)
+def get_connection():
+    return create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
+    fast_executemany = True
+    )
+  
+q1 = 'SELECT * FROM df'
+
+@st.cache
+def read_df1():
+  df1 = pd.read_sql_query(q1, get_connection())
+  return df1
+
+st.write(read_df1)
