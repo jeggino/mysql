71a4f1f697e282c08fa8241fb84d5f26be563b43
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 15:17:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-import sqlalchemy.create_engine
 import streamlit as st
+import pandas as pd
+from sqlalchemy import create_engine
+import time
 
-# Establish a connection to MySQL Server
+SQL_script = st.text_area(label='SQL Input', value='SELECT * FROM TableA')
 
-mydb =create_engine(
-    host="localhost",
-    user="root",
-    password="Platinum79",
-    database="ebird"
+@st.cache(allow_output_mutation=True)
+def get_connection():
+    return create_engine('some random connection string')
 
+@st.cache
+def load_data(SQL_script):
+    with st.spinner('Loading Data...'):
+        time.sleep(0.5)
+        df = pd.read_sql_query(SQL_script, get_connection())
+    return df
 
-)
-mycursor=mydb.cursor()
-print("Connection Established")
-
-st.subheader("Read Records")
-mycursor.execute("select * from df")
-result = mycursor.fetchall()
-for row in result:
-    st.write(row)
+raw_data = load_data(SQL_script)
+raw_data
 
