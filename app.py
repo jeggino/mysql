ffa3d3a581253aac4d5d21cc75cf0fd7317c3a17
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 14:44:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
+import mysql.connector
 import streamlit as st
-import numpy as np
-import pandas as pd
-from sqlalchemy import create_engine
-import pyodbc
-import pymysql
+
+# Establish a connection to MySQL Server
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Platinum79",
+    database="ebird"
 
 
+)
+mycursor=mydb.cursor()
+print("Connection Established")
 
+st.subheader("Read Records")
+mycursor.execute("select * from df")
+result = mycursor.fetchall()
+for row in result:
+    st.write(row)
 
-@st.cache(allow_output_mutation=True)
-def get_connection():
-    return create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
-    fast_executemany = True
-    )
-
-
-
-@st.cache
-def read_df1():
-  df1 = pd.read_sql_query("SELECT * FROM dfd", get_connection())
-  return df1
-  
-
-read_df1()
