00901abb3b58c4f999bb8188b788c97aeb01c8ad
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 13:44:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import sqlalchemy
 import pyodbc
 import pymysql
 
-#creating connection
 
-engine = sqlalchemy.create_engine(
-    "mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
-    )
+
 
 @st.cache(allow_output_mutation=True)
 def get_connection():
-    return create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
+    return sqlalchemy.create_engine(("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
     fast_executemany = True
     )
 
@@ -22,7 +19,7 @@ q1 = 'SELECT * FROM ebird.df'
 
 @st.cache
 def read_df1():
-  df1 = pd.read_sql_query(q1, engine)
+  df1 = pd.read_sql_query(q1, get_connection())
   return df1
   
 
