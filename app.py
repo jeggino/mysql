434943e428334861caf152f570c30cff9044d95c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 12:30:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
-conn = st.experimental_connection("sql") # Config section defined in [connections.sql] in secrets.toml.
+conn = st.experimental_connection("sql") 
+
+df = conn.query("select * from df")
+st.dataframe(df)
