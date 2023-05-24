42cd08c818619224f956b65f4cb70772a7338956
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 13:18:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ import pymysql
 
 
 
-con  = sqlalchemy.create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server")
+# con  = sqlalchemy.create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server")
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="Platinum79",
+                               db="ebird"))
   
-df = pd.read_sql( 'SELECT * FROM df', con=con)
+df = pd.read_sql( 'SELECT * FROM df', con=engine)
 
 st.dataframe(df)
