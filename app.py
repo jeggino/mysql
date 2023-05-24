32465dfc45ece2d27bbce3857f47a62e97550549
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 12:43:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-import streamlit as st
 conn = st.experimental_connection("sql")
-n = st.slider("Pick a number")
-if st.button("Add the number!"):
-    with conn.session as session:
-        session.execute("select * from df")
-        session.commit()
+df = conn.query("select * from df")
+st.dataframe(df)
