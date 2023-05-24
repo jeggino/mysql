648c31a1555ab27c2697cdd91509ae4858d7b790
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 13:45:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sqlalchemy 
+from sqlalchemy import create_engine
 import pyodbc
 import pymysql
 
@@ -10,8 +10,9 @@ import pymysql
 
 @st.cache(allow_output_mutation=True)
 def get_connection():
-    return sqlalchemy.create_engine(("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", fast_executemany = True)
-
+    return create_engine("mssql+pyodbc://username:passowrd@DB_server/database?driver=ODBC+Driver+17+for+SQL+Server", 
+    fast_executemany = True
+    )
 
 q1 = 'SELECT * FROM ebird.df'
 
