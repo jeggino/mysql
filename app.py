4bd84c26a50fe47b7fd07b4550cc831ecae59432
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 12:46:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-conn = st.experimental_connection(
-    "ebird",
-    type="sql",
-    url="mysql://user:pass@localhost:3306/mydb"
-)
+conn = st.experimental_connection("mydb", type="sql", autocommit=True)
 df = conn.query("select * from df")
 st.dataframe(df)
