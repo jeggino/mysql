6c5bfef0d6ce54742127b488cbc81cdfe2dbab6e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-05-24 12:32:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-conn = st.experimental_connection("sql") 
+conn = st.experimental_connection("mydb", type="sql", autocommit=True)
 
 df = conn.query("select * from df")
 st.dataframe(df)
