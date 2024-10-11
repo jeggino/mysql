83d3d600592d3e4efd7ea5845a9e9dda0e613556
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-12 00:51:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from streamlit_gsheets import GSheetsConnection
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read()
+df = conn.read(
+    worksheet="Sheet1",
+    ttl=0,
+)
 
 df
+
+
