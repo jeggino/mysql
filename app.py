8cab9306c14cf7daa9384a2961b89f830ffbb4d6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-11 23:55:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!!!!')
+# Initialize connection.
 conn = st.connection("postgresql", type="sql")
+
+# Perform query.
+df = conn.query('SELECT * FROM mytable;', ttl="10m")
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
