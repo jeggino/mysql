3c64d299551ca9bff51b3aeb3784d986360de9de
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-11 23:10:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,12 @@
 import streamlit as st
-import mysql.connector
 import pandas as pd
+from sqlalchemy import create_engine
 
 
+engine = create_engine("mysql+pymysql://root:Platinum79@localhost/ebird")
 
-config = {
-    'user': 'root',
-    'password': 'Platinum79',
-    'host' : "localhost",
-    'port': 3306,  
-    'database':'ebird'
-}
+query = "SELECT * FROM df"
 
+df = pd.read_sql(query, engine)
 
-db = mysql.connector.connect(**config)
-"""Fetch all records from the 'patients' table."""
-cursor = db.cursor()
-
-# Select the database
-cursor.execute("USE ebird")
-
-# Fetch all patients
-select_patients_query = "SELECT * FROM df"
-cursor.execute(select_patients_query)
-patients = cursor.fetchall()
-
-st.dataframe(patients)
+df
