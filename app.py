602deb984a45731f7d81e5f3ab6883c79ec7d9be
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-11 22:55:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 import streamlit as st
-import SQLAlchemy
-import mysqlclient
+import mysql.connector
+import pandas as pd
+import requests
+from streamlit_lottie import st_lottie
 
-# Initialize connection.
-conn = st.connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from df;')
-st.dataframe(df)
+config = {
+    'user': 'root',
+    'password': 'Platinum79',
+    'host': 'localhost',
+    'port': 3306,  
+    'database':'ebird'
+}
 
+db = mysql.connector.connect(**config)
+"""Fetch all records from the 'patients' table."""
+cursor = db.cursor()
+
+# Select the database
+cursor.execute("USE userdb")
+
+# Fetch all patients
+select_patients_query = "SELECT * FROM patients"
+cursor.execute(select_patients_query)
+patients = cursor.fetchall()
+
+st.dataframe(patients)
