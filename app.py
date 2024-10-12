74381f49baeb3a59f9dc5a3d2d84ca87edeca2b3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-12 08:52:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+# example/st_app.py
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+
 conn = st.connection("gsheets", type=GSheetsConnection)
 
-df = conn.read(
-    worksheet="Sheet2",
-    ttl="10m",
-    nrows=3,
-)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
