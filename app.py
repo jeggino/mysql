084f19fbfbbeb5f33379b2a8fcd6eb8acafca50d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-12 17:24:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 spreadsheet = "https://docs.google.com/spreadsheets/d/1mkF1s_hsoX7GfCdbb_RtaxssqYfLO-kpsJncbqc5Wpw/edit?usp=sharing"
 conn = st.connection("gsheets", type=GSheetsConnection)
-old_data = conn.read(spreadsheet=,ttl=0,worksheet="2087096308")
+old_data = conn.read(spreadsheet=spreadsheet,ttl=0,worksheet="2087096308")
 old_data
 #--- NEW ---
 buurt_choice = ['Bijlmer-West', 'Bijlmer-Centrum', 'Bijlmer-Oost', 'Bos en Lommer',
