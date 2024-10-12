248323c41e1403a67a19a6733cd0ed6d4076e145
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-12 16:01:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,45 @@
 
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
+import pandas as pd
 
 # url = "https://docs.google.com/spreadsheets/d/1mkF1s_hsoX7GfCdbb_RtaxssqYfLO-kpsJncbqc5Wpw/edit?usp=sharing"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-data = conn.read(spreadsheet=st.secrets["spreadsheet"],ttl=0)
-data
+old_data = conn.read(spreadsheet=st.secrets["spreadsheet"],ttl=0)
+
+#--- NEW ---
+buurt_choice = ['Bijlmer-West', 'Bijlmer-Centrum', 'Bijlmer-Oost', 'Bos en Lommer',
+       'Oud-Zuid', 'Osdorp', 'Indische Buurt, Oostelijk Havengebied',
+       'Centrum-West', 'Noord-West', 'Gaasperdam',
+       'Sloterdijk Nieuw-West', 'De Aker, Sloten, Nieuw-Sloten',
+       'Centrum-Oost', 'IJburg, Zeeburgereiland',
+       'Geuzenveld, Slotermeer', 'Oud-Oost', 'Westerpark',
+       'Buitenveldert, Zuidas', 'Weesp, Driemond', 'Noord-Oost',
+       'Oud-West, De Baarsjes', 'De Pijp, Rivierenbuurt', 'Oud-Noord',
+       'Slotervaart', 'Watergraafsmeer']
+
+expertise_choice = ['Geen','Laag','Gemiddeld','Ervaren']
+
+type_bikes = ["Terugtraprem", "Racefiets","Versnellingen buiten","Versnellingen binnen","Vouwfiets","Kinderfiets",
+             "Driewieler","Backfiets - Stuur een foto aub","E-bike - Stuur een foto aub","mijn fiets staat er niet op"]
+
+name = st.text_input("Naam*", placeholder="Vul hier uw naam in ...")
+e_mail = st.text_input("E-mail*", placeholder="Voer hier uw e-mailadres in ...")
+number = st.text_input("Telefoonnummer*", placeholder="Voer hier uw nummer in ...")
+buurt = st.selectbox("Uit welke buurt komt u? (voor statistieken doeleinden)", buurt_choice)
+expertise = st.selectbox("Welke ervaring heeft u met fietsen?", expertise_choice )
+type_bike = st.selectbox("Wat voor fiets wilt u repareren?", type_bikes)
+
+new_data = pd.Dataframe([ 
+                   "Name": name, "e_mail": e_mail, "Phone number": number,
+                   "Neighborhood": buurt, "Expertise": expertise, "Type of bike": type_bike,
+                  }])
+
+updated_df = pd.concat([old_data,new_data],ignore_index=True)
+
+conn.update(worksheet=st.secrets["spreadsheet"], data=updated_df)
+
+
+
+
