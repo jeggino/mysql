66f618bed1f7642c83d484642762a791e3824d53
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-10-13 18:02:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from streamlit_option_menu import option_menu  
 from datetime import datetime as dt
 import random
+from dateutil import parser
 
 
 conn = st.connection("gsheets", type=GSheetsConnection)
